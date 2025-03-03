eb3eec1387f1bea1a7aaba625940a73bca7b62c4
Alan Grossfield <alan_grossfield@urmc.rochester.edu>
2025-03-03 17:27:11 -0500

    remove commented-out constants

--- wham-2d/wham-2d.h
+++ wham-2d/wham-2d.h
@@ -27,8 +27,6 @@ extern double k_B;
 #define RADIANS   6.28318530717959
 
 #define k_B_DEFAULT 0.001982923700 // Boltzmann's constant in kcal/mol K
-//#define k_B  0.0083144621 // Boltzmann's constant kJ/mol-K
-//#define k_B 1.0  // Boltzmann's constant in reduced units
 
 
 
